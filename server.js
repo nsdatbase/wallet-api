4c2d45c3a4697dd148d027ab6dd6a68600ae1a7e
nsdatbase <nsnavshri@gmail.com>
2025-08-28 05:19:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -90,5 +90,6 @@ app.get("/", (req, res) => {
 // ✅ Start server
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(✅ API listening on port ${PORT});
+  console.log(`✅ API listening on port ${PORT}`);
 });
+
